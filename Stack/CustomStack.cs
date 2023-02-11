46c417fe533fa02f696bfb2749559f214744074f
Nikolay Angelov <108483720+nikolaygangelov@users.noreply.github.com>
2023-02-11 23:36:38 +0200

    Add files via upload

---
+++ Stack/CustomStack.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Stack
+{
+    public class CustomStack<T> : IEnumerable<T>
+    {
+        List<T> customList;
+
+        public CustomStack()
+        {
+            customList = new List<T>();
+        }
+
+        public void Push(T element)
+        {
+            customList.Add(element);
+        }
+
+        public void Pop()
+        {
+            if (customList.Count == 0)
+            {
+                throw new InvalidOperationException("No elements");
+            }
+            else
+            {
+                customList.RemoveAt(customList.Count - 1);
+            }
+        }
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = customList.Count - 1; i >= 0; i--)
+            {
+                yield return customList[i];
+            }
+
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+        
+    }
+}
---
+++ Stack/StartUp.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Linq;
+
+namespace Stack
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            CustomStack<int> customStack = new CustomStack<int>();
+            string command = Console.ReadLine();
+
+            if (command == "Pop")
+            {
+                try
+                {
+                    customStack.Pop();
+                }
+                catch (Exception exception)
+                {
+                    Console.WriteLine(exception.Message);
+                }
+            }
+            else
+            {
+                while (command != "END")
+                {
+                    if (command != "Pop")
+                    {
+                        int[] commandIntegers = command
+                            .Split(new string[] {" ", ", " }, StringSplitOptions.RemoveEmptyEntries)
+                            .Select(int.Parse)
+                            .Skip(1)
+                            .ToArray();
+
+                        for (int i = 0; i < commandIntegers.Length; i++)
+                        {
+                            customStack.Push(commandIntegers[i]);
+                        }
+                    }
+                    else if (command == "Pop")
+                    {
+                        try
+                        {
+                            customStack.Pop();
+                        }
+                        catch (Exception exception)
+                        {
+                            Console.WriteLine(exception.Message);
+                        }
+                        
+                    }
+
+                    command = Console.ReadLine();
+                }
+            }
+            for (int i = 0; i < 2; i++)
+            {
+                Console.WriteLine(string.Join(Environment.NewLine, customStack));
+            }
+        }
+    }
+}
