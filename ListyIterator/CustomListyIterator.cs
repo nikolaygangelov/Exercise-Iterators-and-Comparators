4314d27d06cc483f6eda9cdb7b4f3d5233356fec
Nikolay Angelov <108483720+nikolaygangelov@users.noreply.github.com>
2023-02-11 23:03:39 +0200

    Add files via upload

---
+++ ListyIterator/CustomListyIterator.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ListyIterator
+{
+    public class CustomListyIterator<T> : IEnumerable<List<T>>
+    {
+        List<T> customList;
+        public CustomListyIterator(List<T> list)
+        {
+            customList = list;
+            Index = 0;
+        }
+
+        public int Index { get; set; }
+        public bool Move()
+        {
+            if (Index < customList.Count - 1)
+            {
+                Index++;
+                return true;
+            }
+            return false;
+        }
+
+        public bool HasNext()
+        {
+            return Index < customList.Count - 1;
+        }
+
+        public void Print()
+        {
+            if (customList.Count == 0)
+            {
+                throw new InvalidOperationException("Invalid Operation!");
+            }
+            else
+            {
+                Console.WriteLine(customList[Index]);
+            }
+        }
+        public IEnumerator<List<T>> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
---
+++ ListyIterator/StartUp.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ListyIterator
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            List<string> customList = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Skip(1)
+                .ToList();
+            
+            CustomListyIterator<string> customListyOperator = new CustomListyIterator<string>(customList);
+            
+            string command = string.Empty;
+            while ((command = Console.ReadLine()) != "END")
+            {
+                if (command == "Move")
+                {
+                    Console.WriteLine(customListyOperator.Move());
+                }
+                else if (command == "Print")
+                {
+                    try
+                    {
+                        customListyOperator.Print();
+                    }
+                    catch (Exception exception)
+                    {
+
+                        Console.WriteLine(exception.Message);
+                    }
+                    
+                }
+                else if (command == "HasNext")
+                {
+                    Console.WriteLine(customListyOperator.HasNext());
+                }
+            }
+        }
+    }
+}
